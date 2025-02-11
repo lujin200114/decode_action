91889818c08d909a112cd751c906bffb8180885f
action <action@github.com>
2025-02-11 04:27:10 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Tue Feb 11 2025 04:22:10 GMT+0000 (Coordinated Universal Time)
+//Tue Feb 11 2025 04:27:09 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("抖音火山版"),
