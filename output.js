cb26cc0758fb14f396f415b3e311268f4d764440
action <action@github.com>
2025-03-02 07:35:24 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Fri Feb 14 2025 13:14:52 GMT+0000 (Coordinated Universal Time)
+//Sun Mar 02 2025 07:35:24 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("快手极速版"),
