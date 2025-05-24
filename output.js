bb48f67f52eb6d542e7790c1f32bc342d00076f6
action <action@github.com>
2025-05-24 01:42:20 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Thu May 15 2025 10:26:23 GMT+0000 (Coordinated Universal Time)
+//Sat May 24 2025 01:42:20 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("抖音极速版"),
